58641afdef858c15095e39803f40a3e099daa2da
Gabriel Duarte <gabrielqduarte@gmail.com>
2022-07-06 02:08:18 +0200

    fix: improve error messages when theres a generic google error
    Closes #69

--- src/publish.ts
+++ src/publish.ts
@@ -51,11 +51,32 @@ const publish = async (
     extensionId,
     refreshToken,
   })
+  logger.log('Creating zip file...')
 
   const zipFile = createReadStream(asset)
-  const uploadRes = await webStore.uploadExisting(zipFile)
+  logger.log('Uploading zip file to Google Web Store...')
+  const errorMessage = `
 
-  if (uploadRes.uploadState === 'FAILURE') {
+  Unfortunately we can't tell for sure what's the reason for that, but usually this happens when there's something wrong or missing on with the configs.
+  Make sure to check:
+
+  * The extensionId is correctly set on the plugin config
+  * The environment variables GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET, and GOOGLE_REFRESH_TOKEN are correctly set (Double check the authentication guide: https://github.com/GabrielDuarteM/semantic-release-chrome/blob/master/Authentication.md)
+  * Go to https://chrome.google.com/webstore/devconsole, click on the extension you are publishing, and verify that there's no errors there (for example, on the "Why can't I submit" button, or on any of the tabs there)
+
+`
+  let uploadRes
+
+  try {
+    uploadRes = await webStore.uploadExisting(zipFile)
+  } catch (err) {
+    throw new SemanticReleaseError(
+      err as string,
+      `Error uploading extension to Google Web Store. ${errorMessage}  Error details:\n\n`,
+    )
+  }
+
+  if (uploadRes?.uploadState === 'FAILURE') {
     const errors: SemanticReleaseError[] = []
     uploadRes.itemError.forEach((err: any) => {
       const semanticError = new SemanticReleaseError(
@@ -68,9 +89,18 @@ const publish = async (
   }
 
   if (target !== 'draft') {
-    const publishRes = await webStore.publish(target || 'default')
+    logger.log('Publishing extension to Google Web Store...')
+    let publishRes
+    try {
+      publishRes = await webStore.publish(target || 'default')
+    } catch (err) {
+      throw new SemanticReleaseError(
+        err as string,
+        `Error publishing extension to Google Web Store. ${errorMessage}  Error details:\n\n`,
+      )
+    }
 
-    if (!publishRes.status.includes('OK')) {
+    if (!publishRes?.status.includes('OK')) {
       const errors: SemanticReleaseError[] = []
       for (let i = 0; i < publishRes.status.length; i += 1) {
         const code = publishRes.status[i]
@@ -86,6 +116,8 @@ const publish = async (
         throw new AggregateError(errors).errors
       }
     }
+
+    logger.log(`Successfully published extension to Google Web Store`)
   }
 
   return {
