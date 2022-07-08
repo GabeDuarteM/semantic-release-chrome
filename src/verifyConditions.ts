51c949c2bd3ce37ecd9ea985938719f0b7e08de0
Gabriel Duarte <gabrielqduarte@gmail.com>
2022-07-08 02:58:47 +0200

    fix: verify that the credentials are valid before trying to publish
    BREAKING CHANGE: the `verifyConditions` step now requires a
    `extensionId` to be passed in its configs.
    See the README for an updated config example.

--- src/verifyConditions.ts
+++ src/verifyConditions.ts
@@ -1,35 +1,78 @@
 import SemanticReleaseError from '@semantic-release/error'
+import { Context } from 'semantic-release'
+import PluginConfig from './@types/pluginConfig'
+import getEsModule from './getEsModule'
 
 const configMessage = 'Check the README.md for config info.'
 
-const createErrorPATH = (param: string, code: string) =>
+const createErrorEnvFile = (param: string, code: string) =>
   new SemanticReleaseError(
-    `No ${param} specified inside PATH. ${configMessage}`,
+    `Environment variable not found: ${param}. ${configMessage}`,
     code,
   )
 
-const verifyConditions = () => {
-  const {
-    GOOGLE_CLIENT_ID,
-    GOOGLE_CLIENT_SECRET,
-    GOOGLE_REFRESH_TOKEN,
-  } = process.env
+const verifyConditions = async (
+  { extensionId }: PluginConfig,
+  { logger }: Context,
+) => {
+  const { GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET, GOOGLE_REFRESH_TOKEN } =
+    process.env
   const errors: Error[] = []
 
   if (!GOOGLE_CLIENT_ID) {
-    errors.push(createErrorPATH('GOOGLE_CLIENT_ID', 'EGOOGLECLIENTID'))
+    errors.push(createErrorEnvFile('GOOGLE_CLIENT_ID', 'EGOOGLECLIENTID'))
   }
 
   if (!GOOGLE_CLIENT_SECRET) {
-    errors.push(createErrorPATH('GOOGLE_CLIENT_SECRET', 'EGOOGLECLIENTSECRET'))
+    errors.push(
+      createErrorEnvFile('GOOGLE_CLIENT_SECRET', 'EGOOGLECLIENTSECRET'),
+    )
   }
 
   if (!GOOGLE_REFRESH_TOKEN) {
-    errors.push(createErrorPATH('GOOGLE_REFRESH_TOKEN', 'EGOOGLEREFRESHTOKEN'))
+    errors.push(
+      createErrorEnvFile('GOOGLE_REFRESH_TOKEN', 'EGOOGLEREFRESHTOKEN'),
+    )
+  }
+
+  if (!extensionId) {
+    errors.push(
+      new SemanticReleaseError(
+        "Option 'extensionId' was not included in the verifyConditions config. Check the README.md for config info.",
+        'ENOEXTENSIONID',
+      ),
+    )
   }
 
   if (errors.length > 0) {
-    throw new AggregateError(errors).errors
+    const AggregateError = (await getEsModule(
+      'aggregate-error',
+    )) as typeof import('aggregate-error')['default']
+
+    throw new AggregateError(errors)
+  }
+
+  const chromeWebstoreUpload = (await getEsModule(
+    'chrome-webstore-upload',
+  )) as typeof import('chrome-webstore-upload')['default']
+
+  const webStore = await chromeWebstoreUpload({
+    extensionId,
+    clientId: GOOGLE_CLIENT_ID,
+    clientSecret: GOOGLE_CLIENT_SECRET,
+    refreshToken: GOOGLE_REFRESH_TOKEN,
+  })
+
+  let mainErrorMsg: string | undefined
+
+  try {
+    logger.log('Verifying chrome webstore credentials...')
+    await webStore.get()
+    logger.log('Chrome webstore credentials seem to be valid.')
+  } catch (e) {
+    mainErrorMsg =
+      '\n[semantic-release-chrome] Could not connect to Chrome Web Store with the provided credentials. Please check if they are correct.'
+    throw new Error(mainErrorMsg)
   }
 }
 
